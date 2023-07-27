a15767665dd722c7820b8d828e7c745e721c8dfb
Vasudev <112832939+VasuDevaRao333@users.noreply.github.com>
2023-07-27 21:52:02 +0530

    Update

--- TAlphabets.js
+++ TAlphabets.js
@@ -34,3 +34,43 @@ function removeLast() {
   updateDisplay();
 }
 updateDisplay();
+
+
+// Create an array for each set of numbers
+     const evenNumbers = ["ங","ச","ஞ","ந","ம","ர","வ","ள","ன","ஜ","ஹ","க்ஷ","அ","உ","எ","ஏ","ஓ"];
+    const oddNumbers = ["க","ண","ட","த","ப","ய","ல","ழ","ற","ஷ","ஸ","ஆ","இ","ஈ","ஊ","ஐ","ஒ","ஔ"];
+
+    let isEvenSet = true; // Variable to track the current set of numbers being displayed
+    let timer; // Variable to store the timer
+
+    // Function to toggle between even and odd numbers and update the display
+    function toggleNumbers() {
+      const numberDisplay = document.getElementById('numberDisplay');
+      isEvenSet = !isEvenSet;
+      const numbersToShow = isEvenSet ? evenNumbers : oddNumbers;
+      const randomNumber = numbersToShow[Math.floor(Math.random() * numbersToShow.length)];
+      numberDisplay.textContent = randomNumber;
+    }
+
+    // Function to start the timer
+    function startTimer() {
+      let seconds = 0;
+      timer = setInterval(() => {
+        document.getElementById('timerDisplay').textContent = `Time: ${seconds} seconds`;
+        seconds++;
+      }, 1000);
+    }
+
+    // Function to stop the timer
+    function stopTimer() {
+      clearInterval(timer);
+    }
+
+    // Add click event listener to the container to toggle numbers on click
+    document.querySelector('.container').addEventListener('click', () => {
+      if (document.getElementById('numberDisplay').textContent === 'Click to Start') {
+        // Start the timer when the game starts
+        startTimer();
+      }
+      toggleNumbers();
+    });
